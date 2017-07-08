53b78a6a7568ac7eca365b92ee91b44edfcb8e07
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-08 14:31:20 +0700

    oke

--- resources/views/layouts/bannerDesktop.blade.php
+++ resources/views/layouts/bannerDesktop.blade.php
@@ -2,4 +2,15 @@
 	<div>
 		<a href="#"><img src="{{ asset('public/css/desktop3/imghome/home-3.png') }}" alt="" class="center-block"></a>
 	</div>
+	<div>
+		<a href="#"><img src="{{ asset('public/css/desktop3/imghome/slide2.png') }}" alt="" class="center-block"></a>
+	</div>
 </div>
+<script>
+	$('.myBanner').slick({
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	autoplay: true,
+	  	autoplaySpeed: 2000,
+	});	
+</script>
