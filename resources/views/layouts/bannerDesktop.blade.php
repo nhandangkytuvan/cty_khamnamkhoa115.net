60508c60342844e048e61690280d10cec9c8b5cd
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-15 10:41:19 +0700

    ok

--- resources/views/layouts/bannerDesktop.blade.php
+++ resources/views/layouts/bannerDesktop.blade.php
@@ -1,16 +1,19 @@
 <div class="myBanner">
-	<!-- <div>
-		<a href="#"><img src="{{ asset('public/css/desktop3/imghome/home-3.png') }}" alt="" class="center-block"></a>
-	</div> -->
 	<div>
 		<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><img src="{{ asset('public/css/desktop3/imghome/slide2.png') }}" alt="" class="center-block"></a>
 	</div>
+	<div>
+		<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><img src="{{ asset('public/css/desktop3/imghome/slide3.png') }}" alt="" class="center-block"></a>
+	</div>
+	<div>
+		<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><img src="{{ asset('public/css/desktop3/imghome/slide4.png') }}" alt="" class="center-block"></a>
+	</div>
 </div>
 <script>
-	// $('.myBanner').slick({
-	//   	slidesToShow: 1,
-	//   	slidesToScroll: 1,
-	//   	autoplay: true,
-	//   	autoplaySpeed: 2000,
-	// });	
+	$('.myBanner').slick({
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	autoplay: true,
+	  	autoplaySpeed: 3000,
+	});	
 </script>
